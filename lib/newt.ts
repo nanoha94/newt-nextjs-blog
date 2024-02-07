d29a8b628c9999413a91fce793ea250ad73c126c
nanoha <nanoha.creator@gmail.com>
2024-02-07 10:27:25 +0000

    トップページ（記事一覧）を作成

--- lib/newt.ts
+++ lib/newt.ts
@@ -1,8 +1,21 @@
 import "server-only";
 import { createClient } from "newt-client-js";
+import { cache } from "react";
+import type { Article } from "@/types/article";
 
 const client = createClient({
   spaceUid: process.env.NEWT_SPACE_UID + "",
   token: process.env.NEWT_CDN_API_TOKEN + "",
   apiType: "cdn",
+});
+
+export const getArticles = cache(async () => {
+  const { items } = await client.getContents<Article>({
+    appUid: "blog",
+    modelUid: "article",
+    query: {
+      select: ["_id", "_sys", "title", "slug", "meta", "body"],
+    },
+  });
+  return items;
 });
